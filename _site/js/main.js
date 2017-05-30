082fd8f39eab36b6078127ed2bc2fcc7e414c793
Richard Cochrane <screamer42@gmail.com>
2017-05-30 09:26:02 +0200

    Got job-hover working

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function () {
+    function show_job(job_class) {
+        $('.instruction').hide();
+        $('#job-details').html($('#' + job_class).html());
+    }
 
+    function hide_job(job_class) {
+        $('.instruction').show();
+        $('#job-details').html('');
+    }
+
+    $('.big-milestone, .small-milestone').hover(
+        function() {
+            var job_class = $(this).attr('class').split(' ')[1];
+            show_job(job_class);
+        },
+        function() {
+            var job_class = $(this).attr('class').split(' ')[1];
+            hide_job(job_class);
+        }
+    );
+});

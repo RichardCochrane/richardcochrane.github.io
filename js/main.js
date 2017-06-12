7af3a99f5f7b9b43df4917278367a5195cfd66d6
Richard Cochrane <screamer42@gmail.com>
2017-06-12 09:44:20 +0200

    Completed look ’n feel of miscellaneous details

--- js/main.js
+++ js/main.js
@@ -1,22 +1,60 @@
 $(document).ready(function () {
+    function show_misc_details(section) {
+        $(section).siblings('div').show();
+    }
+
+    function hide_misc_details(section) {
+        $(section).siblings('div').hide();
+    }
+
+    function highlight_skills(skills) {
+        $('.bar').addClass('dampen');
+        for (var skill_index in skills) {
+            $('#' + skills[skill_index]).removeClass('dampen');
+        }
+    }
+
+    function reset_skills_highlight() {
+        $('.dampen').removeClass('dampen');
+    }
+
     function show_job(job_class) {
         $('.instruction').hide();
         $('#job-details').html($('#' + job_class).html());
+
+        var skills = $('#' + job_class + ' .description').data('skills').split(',');
+
+        highlight_skills(skills);
     }
 
     function hide_job(job_class) {
         $('.instruction').show();
         $('#job-details').html('');
+
+        reset_skills_highlight();
     }
+
+    $('.misc-details header').hover(
+        function() {
+            show_misc_details(this);
+        },
+        function() {
+            // hide_misc_details(this);
+        }
+    );
 
     $('.big-milestone, .small-milestone').hover(
         function() {
             var job_class = $(this).attr('class').split(' ')[1];
-            show_job(job_class);
+            if (job_class !== 'next') {
+                show_job(job_class);
+            }
         },
         function() {
             var job_class = $(this).attr('class').split(' ')[1];
-            hide_job(job_class);
+            if (job_class !== 'next') {
+                hide_job(job_class);
+            }
         }
     );
 });
